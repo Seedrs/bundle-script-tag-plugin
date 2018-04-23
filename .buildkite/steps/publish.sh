cc62bb81c83168443baf932181793387bf72997c
kenleytomlin <kenleytomlin@gmail.com>
2018-04-23 14:51:55 +0100

    ci(.buildkite): Fix publishing to github and npm

--- .buildkite/steps/publish.sh
+++ .buildkite/steps/publish.sh
@@ -5,6 +5,11 @@ set -euo pipefail
 mkdir ~/.ssh
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
 
+git config user.email "development.team@seedrs.com"
+git config user.name "seedrs.bot"
+
+git checkout fix-pipline-publish-step
+
 yarn build && \
 yarn semantic-release
 
